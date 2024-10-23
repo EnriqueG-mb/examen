35cf35ac2e27954bb6bc4962e831448a1f3a28c3
Jorge Enrique Garcia Gonzalez <je.garcia@softtekprojects.com>
2024-10-22 18:02:25 -0600

    se termino

---
+++ src/main/java/com/example/talleracademia/TalleracademiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.talleracademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalleracademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalleracademiaApplication.class, args);
+	}
+
+}
